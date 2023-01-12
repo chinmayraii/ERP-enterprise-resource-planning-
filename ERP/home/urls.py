5a948347a1873d33eca90c14ce3ea3f4196b7013
CHINMAY RAI <114671850+chinmayraii@users.noreply.github.com>
2023-01-13 00:55:11 +0530

    update student info

--- ERP/home/urls.py
+++ ERP/home/urls.py
@@ -4,11 +4,17 @@ from . import views
 urlpatterns = [
     path('',views.course),
     path('showallcourse',views.showallcourse),
+    path('addcourse',views.addcourse),
+    path('searchcourse',views.searchcourse),
+    path('deletecourse',views.deletecourse),
+    path('updatecourse',views.updatecourse),
+    path('up_course/<int:uid>/',views.up_course),
+    path('student',views.student),
     path('addstudent',views.addstudent),
     path('showallstudent',views.showallstudent),
     path('deletestudent',views.deletestudent),
     path('searchstudent',views.searchstudent),
     path('updatestudent',views.updatestudent),
-    path('up_course/<int:uid>/',views.up_course),
+    path('up_student/<int:uid>/',views.up_student),
 
 ]
--- ERP/home/views.py
+++ ERP/home/views.py
@@ -2,58 +2,111 @@ from django.shortcuts import render
 from .models import *
 from django.contrib import messages
 from django.db.models import Q
+import datetime
 
 def course(request):
-    return render (request,('course.html'))
-
-def showallstudent(request):
-    data=Student.objects.all()
-    return render (request,'student.html',{'data':data}) 
+    cou=Course.objects.all()
+    return render (request,'course.html',{'cou':cou}) 
 
 def showallcourse(request):
-    stu=Course.objects.all()
-    return render (request,'course.html',{'stu':stu})        
+    cou=Course.objects.all()
+    return render (request,'course.html',{'cou':cou}) 
+
+def addcourse(request):
+    cname=request.POST['cname']
+    duration=request.POST['duration']
+    detail=request.POST['detail']
+    fees=request.POST['fees']
+    fees=int(fees)
+    if Course.objects.filter(cname=cname).exists():
+        messages.error(request,'Course already exists ')
+        cou=Course.objects.all()
+        return render(request,'course.html',{'cou':cou}) 
+    else:
+        Course.objects.create(cname=cname,duration=duration,details=detail,fees=fees)
+        messages.success(request,'Sucessfully Added')
+        cou=Course.objects.all()
+        return render(request,'course.html',{'cou':cou}) 
+
+def searchcourse(request):
+    find=request.POST['name']
+    c=Course.objects.filter(Q(cname=find) | Q(duration=find) | Q(details=find)).all()
+    return render(request,'course.html',{'cou':c}) 
+
+def deletecourse(request):
+    cid=request.GET['cid']
+    Course.objects.get(id=cid).delete()
+    messages.success(request,'!!!!!! Sucessfully Deleted !!!!!')
+    cou=Course.objects.all()
+    return render (request,'course.html',{'cou':cou}) 
+
+def updatecourse(request):
+    c=Course()
+    c.id=request.POST['id']
+    c.cname=request.POST['cname']
+    c.duration=request.POST['duration']
+    c.details=request.POST['details'] 
+    fees=request.POST['fees']
+    c.fees=int(fees)
+    c.save()
+    messages.success(request,'!!!!! Course Updated Successfully !!!!')
+    cou=Course.objects.all()
+    return render (request,'course.html',{'cou':cou})
+
+def up_course(request,uid):
+    res=Course.objects.get(id=uid)
+    return render(request,'updatecourse.html',{'i':res})                               
+
+def showallstudent(request):
+    stu=Student.objects.all()
+    cou=Course.objects.all()
+    return render (request,'student.html',{'stu':stu,'cou':cou}) 
+
+def student(request):
+    stu=Student.objects.all()
+    cou=Course.objects.all()
+    return render (request,'student.html',{'stu':stu,'cou':cou})     
 
 def addstudent(request):
-    name=request.POST['name']
-    branch=request.POST['branch']
-    course=request.POST['course']
+    s=Student()
+    s.sname=request.POST['sname']
+    s.branch=request.POST['branch']
+    sid=request.POST['course']
+    s.course=Course.objects.get(id=sid)
     mob=request.POST['mob']
-    mob=int(mob)
-    email=request.POST['email']
-    qualification=request.POST['qualification']
+    s.mob=int(mob)
+    s.email=request.POST['email']
+    s.qualification=request.POST['qualification']
     passoutyear=request.POST['passoutyear']
-    passoutyear=int(passoutyear)
-    status=request.POST['status']
-    address=request.POST['address']
-    if Student.objects.filter(email=email).exists():
-        messages.error(request,'Email already exists')
-        data=Student.objects.all()
-        return render (request,'student.html',{'data':data})    
-    else:
-        Student.objects.create(name=name,branch=branch,course=course,mob=mob,email=email,qualification=qualification,passoutyear=passoutyear,status=status,address=address)
-        messages.success(request,'sucessfully added !!!!')
-        data=Student.objects.all()
-        return render (request,'student.html',{'data':data}) 
+    s.passoutyear=int(passoutyear)
+    s.sem=request.POST['sem']
+    s.date=datetime.datetime.now()
+    s.status=request.POST['status']
+    s.address=request.POST['address']
+    s.save()
+    stu=Student.objects.all()
+    cou=Course.objects.all()
+    return render (request,'student.html',{'stu':stu,'cou':cou}) 
 
 def deletestudent(request):
     sid=request.GET['sid']
     Student.objects.get(id=sid).delete()
-    data=Student.objects.all()
-    return render(request,"student.html",{'data':data}) 
+    stu=Student.objects.all()
+    return render(request,"student.html",{'stu':stu}) 
 
 def searchstudent(request):
-    find=request.POST['name']
-    s=Student.objects.filter(Q(name=find) | Q(email=find) | Q(course=find) | Q(branch=find) | Q(status=find) | Q(qualification=find)).all()
-    return render(request,"student.html",{'data':s})
+    find=request.POST['stuname']
+    s=Student.objects.filter(Q(sname=find) | Q(email=find) | Q(course=find) | Q(branch=find) | Q(status=find) | Q(qualification=find)).all()
+    return render(request,"student.html",{'stu':s})
 
 
 def updatestudent(request):
     s=Student()
     s.id=request.POST['id']
-    s.name=request.POST['name']
+    s.sname=request.POST['sname']
     s.branch=request.POST['branch']
-    s.course=request.POST['course']
+    sid=request.POST['course']
+    s.course=Course.objects.get(id=sid)
     s.email=request.POST['email']
     mob=request.POST['mob']
     s.mob=int(mob)
@@ -63,9 +116,10 @@ def updatestudent(request):
     s.status=request.POST['status']
     s.address=request.POST['address']
     s.save()
-    data=Student.objects.all()
-    return render (request,'student.html',{'data':data})
+    cou=Course.objects.all()
+    stu=Student.objects.all()
+    return render (request,'student.html',{'stu':stu,'cou':cou})
 
-def up_course(request,uid):
+def up_student(request,uid):
     res=Student.objects.get(id=uid)
     return render(request,'updatestudent.html',{'i':res})                    
